e6665d3f0acbe597e43bd6a9ccb0600eab3e8278
Dũng <macone@Dungs-Macbock.local>
2025-12-26 08:38:43 +0700

    deploy flask resend email

--- project/app.py
+++ project/app.py
@@ -105,5 +105,7 @@ def resend():
     return jsonify({"status": "success"})
 
 
+# if __name__ == "__main__":
+#     app.run(debug=True)
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
